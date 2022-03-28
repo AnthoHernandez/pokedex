c416e5a04c2b56d0eba85e04b4673943b7431bdf
AnthoHernandez <99144623+AnthoHernandez@users.noreply.github.com>
2022-03-28 00:16:38 -0600

    update background
    and css

--- assets/js/pokedex.js
+++ assets/js/pokedex.js
@@ -6,7 +6,9 @@ const fetchPokemon = () => {
     fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
+            pokeN("Pokemon no encontrado")
             pokeImage("./assets/images/pokemon-sad.gif")
+
         }
         else {
             return res.json();
@@ -16,6 +18,16 @@ const fetchPokemon = () => {
             console.log(data);
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
+
+            let pokeNameR = data.name;
+            pokeNameR=pokeNameR.toUpperCase();
+            pokeN(pokeNameR);
+
+            let pokID = data.id;
+            pokeID(pokID);
+            let ptype = data.types[0].type.name;
+            pokeType(ptype);
+
             console.log(pokeImg);
         }
     });
@@ -25,5 +37,17 @@ const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
 }
-
-
+const pokeN = (String) => {
+    
+    const pokeNameR = document.getElementById("pokeNameR");
+    pokeNameR.textContent = String;
+  
+}
+function pokeID(String){
+    const id = document.getElementById("pokeNumero");
+    id.textContent = "N°" + String ;
+}
+function pokeType(String){
+    const type = document.getElementById("pokeTipo");
+    type.textContent = "Tipo" + String ;
+}
